d49eebcfdb4babb11ccc49dc09d1ab9e785df32e
라효은 <sophia4338@naver.com>
2022-12-04 22:16:33 +0900

    점의_위치_구하기

---
+++ 프로그래머스/점의 위치 구하기.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ dot:[Int]) -> Int {
+    if dot[0] > 0 && dot[1] > 0 {
+        return 1
+    }
+    else if dot[0] > 0 && dot[1] < 0 {
+        return 4
+    }
+    else if dot[0] < 0 && dot[1] < 0 {
+        return 3
+    }
+    else {
+        return 2
+    }
+}
