eadbd6e8826377b2cad2e3f42d0dd64c7729859d
hyoeunla <sophia4338@naver.com>
2023-04-25 15:14:52 +0900

    2753

---
+++ 백준/2753.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+let year = readLine()!
+let num = Int(year)!
+if (num % 4 == 0 && num % 100 != 0) {
+    print(1)
+} else if (num % 400 == 0) {
+    print(1)
+} else {
+    print(0)
+}
+
